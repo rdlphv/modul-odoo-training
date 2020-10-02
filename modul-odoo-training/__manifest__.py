05ac92d780edb40a52b7c1af8271217d77a2f1a0
Rodolphe_ci <rdlphv@gmail.com>
2020-10-02 00:54:50 +0000

    initial module creation

---
+++ modul-odoo-training/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Training",
+
+    'summary': """
+        IT4LIFE Test de recrutement Odoo
+        Module de gestion de sessions d'apprentissage""",
+
+    'description': """
+        Module de gestion de sessions d'apprentissage
+    """,
+
+    'author': "Rodolphe_ci",
+    'website': "https://www.linkedin.com/in/rodolphe-ci",
+    'maintainer': 'Rodolphe CHABI ILOUGBADE'
+    'category': 'Training',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'application': True,
+    'auto_install': False,
+    'installable': True,
+    'image': [
+        'static/description/icon.jpg',
+    ],
+}
